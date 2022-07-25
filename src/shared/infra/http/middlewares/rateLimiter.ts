34b7c7549ef114f1f7e5edc04e227b40cbc9ff23
Pamploni Cadé <pamplonicade@MacBook-Air-de-Pamploni.local>
2022-07-25 13:44:20 -0300

    README.md - Incluído restante das instruções 4

--- src/shared/infra/http/middlewares/rateLimiter.ts
+++ src/shared/infra/http/middlewares/rateLimiter.ts
@@ -1,21 +1,21 @@
 import { Request, Response, NextFunction } from 'express';
 import { RateLimiterRedis } from 'rate-limiter-flexible';
-import redis from 'redis';
+//import redis from 'redis';
 import AppError from '@shared/errors/AppError';
 
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: Number(process.env.REDIS_PORT),
-  password: process.env.REDIS_PASS || undefined,
-});
+// const redisClient = redis.createClient({
+//   host: process.env.REDIS_HOST,
+//   port: Number(process.env.REDIS_PORT),
+//   password: process.env.REDIS_PASS || undefined,
+// });
 
-const limiter = new RateLimiterRedis({
-  storeClient: redisClient,
-  keyPrefix: 'ratelimit',
-  points: 5, // Number of points
-  duration: 1, // Per second
-  blockDuration: 60 * 15,
-});
+// const limiter = new RateLimiterRedis({
+//   storeClient: redisClient,
+//   keyPrefix: 'ratelimit',
+//   points: 5, // Number of points
+//   duration: 1, // Per second
+//   blockDuration: 60 * 15,
+// });
 
 export default async function rateLimiter(
   request: Request,
